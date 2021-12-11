1d91e6a08ab3841d9d020c50b66e7f4fa366ba52
3quarterscolon <94267030+3quarterscolon@users.noreply.github.com>
2021-12-11 19:33:42 +0000

    Add board and get input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = [' '; 9];
+    let mut turn = true;
+    for row in 1..4 {
+        println!("\n   |   |");
+        for col in 1..4 {
+            print!(
+                "{hori_line}{owner}{hori_line}{vert_line}", 
+                owner = board[row * col - 1], 
+                hori_line = if row != 3 { "_" } else { " " },
+                vert_line = if col != 3 { "|" } else { "" }
+            );
+        }
+    }
+
+    println!(
+        "\n{}'s turn.\nMove?",
+        if turn {"X"} else {"O"}
+    );
+
+    let mut player_move = String::new();
+
+    io::stdin()
+        .read_line(&mut player_move)
+        .expect("Input failed");
+    
+    // let player_move = [];
 }
